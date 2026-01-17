f827643aa8c844699afa5237f704ece9f154f9b4
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2026-01-17 14:39:52 +0100

    build: add build_verify dependency and ensure build test

---
+++ test/ensure_build_test.dart
@@ -0,0 +1,9 @@
+import 'package:build_verify/build_verify.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'Ensure that the build is clean (i.e. no generated files are out of date)',
+    expectBuildClean,
+  );
+}
