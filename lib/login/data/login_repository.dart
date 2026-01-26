43404dc6fac3c3e8e12f0b7f8d79bbeca774062c
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2026-01-26 16:16:27 +0100

    refactor(http): simplify NetworkRequestExecutor API

--- lib/login/data/login_repository.dart
+++ lib/login/data/login_repository.dart
@@ -1,6 +1,5 @@
 import 'package:cafe_analog_app/core/failures.dart';
 import 'package:cafe_analog_app/core/network_request_executor.dart';
-import 'package:cafe_analog_app/generated/api/client_index.dart';
 import 'package:cafe_analog_app/generated/api/coffeecard_api_v2.enums.swagger.dart';
 import 'package:cafe_analog_app/generated/api/coffeecard_api_v2.models.swagger.dart';
 import 'package:cafe_analog_app/login/data/authentication_tokens.dart';
@@ -9,12 +8,9 @@ import 'package:fpdart/fpdart.dart';
 /// Handles data operations related to user login.
 class LoginRepository {
   const LoginRepository({
-    required CoffeecardApiV2 apiV2,
     required NetworkRequestExecutor executor,
-  }) : _apiV2 = apiV2,
-       _executor = executor;
+  }) : _executor = executor;
 
-  final CoffeecardApiV2 _apiV2;
   final NetworkRequestExecutor _executor;
 
   /// Requests a magic link to be sent to the provided email.
@@ -24,7 +20,7 @@ class LoginRepository {
       loginType: LoginType.app.value,
     );
     return _executor
-        .execute(() => _apiV2.accountLoginPost(body: request))
+        .run((api) => api.v2.accountLoginPost(body: request))
         .map((_) => unit);
   }
 
@@ -32,7 +28,7 @@ class LoginRepository {
   TaskEither<Failure, AuthTokens> authenticateWithMagicLinkToken(String token) {
     final request = TokenLoginRequest(token: token);
     return _executor
-        .execute(() => _apiV2.accountAuthPost(body: request))
+        .run((api) => api.v2.accountAuthPost(body: request))
         .map(
           (response) => AuthTokens(
             jwt: response.jwt,
